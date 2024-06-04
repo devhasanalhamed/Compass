bdb8dc40f448dbf06de6fce502988ac1fdd1b4fd
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-06-05 01:06:11 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/compass/MainActivity.java
+++ app/src/main/java/com/example/compass/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.compass;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final ActivityResultLauncher<String> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+        @Override
+        public void onActivityResult(Boolean o) {
+            if (o) {
+//                getLocation();
+            }
+        }
+    });
+
+    private SensorManager sensorManager;
+
+    Sensor accelerometer, magnetometer, pressureSensor;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    private Location currentLocation;
+
+    private float[] gravity;
+
+    private float[] geomagnetic;
+
+    private float currentDegree = 0f;
+
+//    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
